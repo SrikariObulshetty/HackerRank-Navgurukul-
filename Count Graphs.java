7f01e3cfc6af74b78bd699f88cfb111c0a2b83a0
Srikari Obulshetty <srikariobulshetty@gmail.com>
2025-01-28 21:48:45 +0530

    Create Count Graphs.java

---
+++ Count Graphs.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        System.out.println ( (int)Math.pow(2, (n *(n-1))/2));
+    }
+}
